9c91ed2fec882c2099c5c765898f6639d550eead
Petr Kučera <petr@khome.cz>
2022-03-21 22:13:53 +0000

    Update style

--- components/layout.js
+++ components/layout.js
@@ -4,22 +4,21 @@ import Meta from "../components/meta";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-zinc-800 text-white">
       <Meta></Meta>
-      <header className="bg-fuchsia-100 mb-8 py-4">
+      <header className="bg-zinc-900 mb-8 py-4">
         <div className="container mx-auto flex justify-center">
           <Link href="/">
             <a>
               <IconTree />
             </a>
           </Link>
-          <span className="mx-auto">Welcome to my blog</span>{" "}
         </div>
       </header>
       <main className="container mx-auto flex-1">{children}</main>
-      <footer className="bg-fuchsia-100 mt-8 py-4">
+      <footer className="bg-zinc-900 mt-8 py-20">
         <div className="container mx-auto flex justify-center">
-          &copy; 2022 Petr Kucera
+          &copy; 2022 Petr Kučera
         </div>
       </footer>
     </div>
