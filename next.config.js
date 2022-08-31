e7c190883637832c7e69c8c7a5eb5a9c1d0f5ad9
petrkucerak <petr@khome.cz>
2022-08-31 16:36:40 +0200

    Config PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-};
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  trailingSlash: true,
+  pwa: {
+    dest: "public",
+    // cacheOnFrontEndNav: true, // make all page linked form index cached
+    disable: process.env.NODE_ENV === "development",
+    register: false,
+    runtimeCaching,
+  },
+});
