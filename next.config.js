1d6b31fa40db9f2da1d2fd43bebc5da634be6253
Petr Kučera <petr@khome.cz>
2022-08-31 16:52:09 +0200

    Pwa (#12)
    * Add next-pwa library
    
    * Config PWA
    
    * Add metadata
    
    * Add icons
    
    * PWA workers
    
    * Next.config with PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-};
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/',
+  
+  //...
+})
+
+module.exports = withPWA({
+  // next.js config
+})
