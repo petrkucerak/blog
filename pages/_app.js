42d6f5c49b30b5b2a1c0d4dbfbfca460fac3972f
Petr Kučera <petr@khome.cz>
2023-02-03 16:02:57 +0100

    Fix warnings & improve style (#35)
    * Install the `highlighte.js`
    
    * Update packages version
    
    * Fix dependence errors
    
    * Install font by new NexJS method
    
    * Set up syntax highlighter

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from "../components/layout";
 import "../styles/globals.css";
+import { Athiti } from "@next/font/google";
+
+const athiti = Athiti({
+  weight: ["500", "700"],
+  subsets: ["latin-ext", "latin"],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --athiti-font: ${athiti.style.fontFamily};
+          }
+        `}
+      </style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
