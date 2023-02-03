c571fb8b652a9f2d7cf93cf2376f71fb2a9b46e3
Petr Kucera <petr@khome.cz>
2023-02-03 15:57:32 +0100

    Set up syntax highlighter

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,23 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /hljs+/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
         athiti: ["var(--athiti-font)", "sans-serif"],
       },
     },
+    hljs: {
+      theme: "night-owl",
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-highlightjs"),
+  ],
 };
