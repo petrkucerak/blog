fdef4633d38323309c1afa856d62cc55a37c1ee8
Petr Kucera <petr@khome.cz>
2023-02-03 17:38:55 +0100

    Wrap tags under the post

--- components/tags.js
+++ components/tags.js
@@ -1,6 +1,6 @@
 export default function Tags({ tags }) {
   return (
-    <div>
+    <div className="flex flex-wrap">
       {tags.map((tag) => {
         return <span key={tag} className="mx-2 text-stone-400	">{"#"+tag}</span>;
       })}
