e997163fd855ff18acb18670856a61fb6bf3e50a
Petr Kucera <petr@khome.cz>
2023-02-03 15:18:39 +0100

    Install the `highlighte.js`

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import md from "markdown-it";
+import hljs from "highlight.js";
 
 export async function getStaticPaths() {
   const files = fs.readdirSync("posts");
@@ -30,31 +31,41 @@ export default function PostPage({ frontmatter, content }) {
   return (
     <div className="prose prose-lg prose-stone max-w-4xl w-[90vw] m-0 mx-auto">
       <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{
-        __html: md({
-          html: true,        // Enable HTML tags in source
-          xhtmlOut: false,        // Use '/' to close single tags (<br />).
-          // This is only for full CommonMark compatibility.
-          breaks: false,        // Convert '\n' in paragraphs into <br>
-          linkify: true,        // Autoconvert URL-like text to links
+      <div
+        dangerouslySetInnerHTML={{
+          __html: md({
+            html: true, // Enable HTML tags in source
+            xhtmlOut: false, // Use '/' to close single tags (<br />).
+            // This is only for full CommonMark compatibility.
+            breaks: false, // Convert '\n' in paragraphs into <br>
+            linkify: true, // Autoconvert URL-like text to links
 
-          // Enable some language-neutral replacement + quotes beautification
-          // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
-          typographer: false,
+            // Enable some language-neutral replacement + quotes beautification
+            // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
+            typographer: false,
 
-          // Double + single quotes replacement pairs, when typographer enabled,
-          // and smartquotes on. Could be either a String or an Array.
-          //
-          // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-          // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-          quotes: '„“‚‘',
+            // Double + single quotes replacement pairs, when typographer enabled,
+            // and smartquotes on. Could be either a String or an Array.
+            //
+            // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+            // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+            quotes: "„“‚‘",
 
-          // Highlighter function. Should return escaped HTML,
-          // or '' if the source string is not changed and should be escaped externally.
-          // If result starts with <pre... internal wrapper is skipped.
-          highlight: function (/*str, lang*/) { return ''; }
-        }).render(content)
-      }} />
+            // Highlighter function. Should return escaped HTML,
+            // or '' if the source string is not changed and should be escaped externally.
+            // If result starts with <pre... internal wrapper is skipped.
+            highlight: function (str, lang) {
+              if (lang && hljs.getLanguage(lang)) {
+                try {
+                  return hljs.highlight(str, { language: lang }).value;
+                } catch (__) {}
+              }
+
+              return ""; // use external default escaping
+            },
+          }).render(content),
+        }}
+      />
     </div>
   );
 }
