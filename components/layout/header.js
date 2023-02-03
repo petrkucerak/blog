ce055c46fdf3ef9ec78297f967cf3e15bdc0b8c9
Petr Kucera <petr@khome.cz>
2023-02-03 16:38:23 +0100

    Create title

--- components/layout/header.js
+++ components/layout/header.js
@@ -4,8 +4,9 @@ export default function Header() {
   return (
     <header className="bg-stone-900 mb-8 py-4">
       <div className="container mx-auto flex justify-center">
-        <Link href="/" title="Kůčův blog">
+        <Link href="/" title="Kůčův blog" className="contents">
           <IconTree className="text-white" />
+          <h1 className="text-white font-semibold uppercase text-lg ml-1">Kůčův blog</h1>
         </Link>
       </div>
     </header>
